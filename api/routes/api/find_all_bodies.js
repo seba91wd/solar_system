f0fd448d8ca60fdd32a0c2239ffc1bb06ff473c2
seba91wd <seba91120@gmail.com>
2023-08-30 20:53:14 +0200

    30/08 update

--- api/routes/api/find_all_bodies.js
+++ api/routes/api/find_all_bodies.js
@@ -8,14 +8,41 @@ module.exports = (app) => {
     app.get('/api/bodies', (req, res) => {
         console.log("find_all_bodies");
 
-        if (req.query.name) {
-            const name = req.query.name;
-            
-            if (name.length <2) {
-                const message = "Le therme de recherce doit contenir au moin 2 caractère";
-                return res.status(400).json({message})
-            }
-            const limit = parseInt(req.query.limit) || 8
+        if (req.query.name || req.query.id || req.query.type) {
+            const name = req.query.name || null;
+            const type = req.query.type;
+            const id = parseInt(req.query.id);
+
+            if (name && name.length < 2) {
+                const message = "Le terme de recherche doit contenir au moins 2 caractères";
+                return res.status(400).json({ message });
+            };
+
+            let where_clause = {};
+
+            if (name) {
+                where_clause.name = {
+                    [Op.like]: `%${name}%`
+                };
+            };
+
+            if (type && valide_types.includes(type)) {
+                where_clause.body_type = type;
+            };
+
+            if (id) {
+                return bodie.findByPk(id).then(corps => {
+                    if (corps === null) {
+                        const message = "Le corps céleste demandé n'existe pas, réessayer avec un autre ID";
+                        return res.status(404).json({ message });
+                    }
+                    const message = `Il y a 1 corps céleste qui correspond à l'ID ${corps.id}.`;
+                    res.json({ message, corps });
+                });
+            };
+
+            const limit = parseInt(req.query.limit) || 10;
+
             return bodie.findAndCountAll({
                 where: where_clause,
                 order: ['id'],
@@ -44,15 +71,16 @@ module.exports = (app) => {
         } 
         else {
             // Récupération de la liste complète des corps célestes
-            bodie.findAndCountAll({order: ['id']})
-            .then(({count, rows}) => {
-                const message = `La liste des corps célestes a bien été récupérée, ${count} objets trouvés`;
-                res.json({ message, data: rows });
-            })
-            .catch(error => {
-                const message = "La liste des corps célestes n'a pas pu être récupérée. Réessayer dans quelques instants.";
-                res.status(500).json({message, data: error});
-            });
+            const limit = parseInt(req.query.limit) || null; // null = pas de limite de résultat
+            bodie.findAndCountAll({ order: ['id'], limit: limit })
+                .then(({ count, rows }) => {
+                    const message = `La liste des corps célestes a bien été récupérée, ${count} objets trouvés`;
+                    res.json({ message, data: rows });
+                })
+                .catch(error => {
+                    const message = "La liste des corps célestes n'a pas pu être récupérée. Réessayer dans quelques instants.";
+                    res.status(500).json({ message, data: error });
+                });
         };
     });
 };
